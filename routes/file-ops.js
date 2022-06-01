11d5a0285a7ad59e082938428275a1178fe24790
Varkous <varkous@protonmail.com>
2022-06-01 13:23:37 -0700

    Change convertURL route, allow audio only parsing

--- routes/file-ops.js
+++ routes/file-ops.js
@@ -10,6 +10,7 @@ const {checkModeType} = require('../scripts/Helpers.js');
 const axios = require('axios');
 const ytdl = require('ytdl-core');
 const mime = require('mime-types');
+const child_process = require("child_process");
 
 let partition = process.env.partition || 'public';
 const UsersDirectory = process.env.UsersDirectory || 'users';
@@ -71,7 +72,19 @@ module.exports = {
     else return await Rename(req, res, oldpath, newpath);
   })),
 
+  setMetadata: async function (req, res, metadata, filePath) {
+    let metaOptions = [];
+    for (let entry in metadata) {
+      metaOptions.push(`--${entry}`);
+      metaOptions.push(metadata[entry]);
+    }
+    metaOptions.push(filePath);
+    Sessions.user(req, req.session).operation = false;
+    fs.chownSync(filePath, req.session.user.uid, 100);
 
+    console.log(`${metaOptions.join(' ')} ${filePath}`);
+    return child_process.spawn('id3v2', metaOptions, {detached: true});
+  },
   /* ====================================
   If user authentication was granted, and directory access was granted from posting to "/upload", this is the NEXT request, which uploads the files to the folder information stored in req.params.
   ======================================= */
@@ -79,88 +92,93 @@ module.exports = {
 
    await AccessDirectory(req, req, next);
    let newfolders = req.body.newfolders ? Array.from(req.body.newfolders) : '';
-   const {name, url} = req.body;
+   let {name, url, audioOnly, metadata} = req.body;
    let file = {name: name, path: req.params.folder + req.params[0]};
+   // If user selected Audio Only, parse only audio data
    let dirpath = path.resolve(req.session.home, file.path);
 
-   fs.readdir(dirpath, (err, entries) => { // See if entry already exists
+  fs.readdir(dirpath, (err, entries) => { // See if entry already exists
    	entries = entries.map( e => e.substring(e.indexOf('.'), 0)); // Remove extension from each entry, our new file does not have one
-  	if (err) return false;
+  	if (err || !entries) return false;
   	else if (entries.includes(file.name))
 	  return ReportData(req, res, false, {
         content: [`${EntryToHTML(file, 'darkcyan')} already exists within ${EntryToHTML(file.path, '#22a0f4')}, use different file name or different folder`],
         type: 'error'
       });
 
-	// ---------------------------------------------------------------------------------
-	new Promise( async (resolve, reject) => {
+// ---------------------------------------------------------------------------------
+  	new Promise( async (resolve, reject) => {
+      if (!ytdl.validateURL(url)) return reject(new Error(`Failed to acquire video ID from URL`));
 
-	  if (url.includesAny('youtube', 'youtu.be')) { //A vast majority of any conversion uploads will likely be from the site YouTube, which throttles data stream requests. Special module and conditions are setup to allow proper retrieval of data.
+      let info = await ytdl.getInfo(url); // Lots of info indeed
+      let filePath = `${dirpath}/${file.name}${audioOnly ? '.mp3' : '.mp4'}`;
+      let audioFormat = audioOnly ? info.formats[info.formats.length - 4] : false;
 
-	    if (!ytdl.validateURL(url)) return reject(new Error(`Failed to acquire video ID from URL`));
+      if (url.includesAny('youtube', 'youtu.be')) { //A vast majority of any conversion uploads will likely be from the site YouTube, which throttles data stream requests. Special module and conditions are setup to allow proper
+  	    let contentLength = await parseInt(audioOnly ? audioFormat.contentLength : info.formats[0].contentLength);
+        // For browser progress bar. If audio only, the fourth-to-last element of formats is the mp4 audio content length we want (mp3). If video AND audio (mp4), the first element in formats is default.
+// ---------------------------------------------------------------------------------
+  	    if (!contentLength) return reject(new Error(`File info could not be parsed, conversion rejected. Try using file address source, or a different URL`));
 
-	    let info = await ytdl.getInfo(url); // Lots of info indeed
-	    file.name = file.name + '.mp4' ; // If user did not list as video, just load as audio data
-	    let contentLength = await parseInt(info.formats[0].contentLength); // For browser progress bar
-
-	    if (!contentLength) return reject(new Error(`File info could not be parsed, conversion rejected. Try using file address source, or a different URL`));
-
-	    res.setHeader('content-type', 'application/octet-stream');
-  		res.setHeader('content-length', contentLength);
-  		res.setHeader('filename', file.name);
-  		
-	    ytdl(url).pipe(fs.createWriteStream(`${dirpath}/${file.name}`)).on("finish", () => {
-          fs.chown(`${dirpath}/${file.name}`, req.session.user.uid, 100, (err) => err ? console.log(err) : false);
+        ytdl(url, {quality: audioOnly ? 'highestaudio' : 'highest' })
+        .pipe(fs.createWriteStream(filePath)).on("finish", () => {
+          // module.exports.setMetadata(req, res, metadata, filePath).on('exit', function() {
+          res.setHeader('content-type', 'application/octet-stream');
+          res.setHeader('content-length', contentLength);
+          res.setHeader('filename', file.name);
+            fs.createReadStream(filePath).pipe(res).on("error", () => reject(new Error(`Data stream interrupted. File could not be sent for download, refresh directory and locate file to download.`))); // Error catch for file stream to response for download
+            return resolve(true);
+          // }).on("error", () => console.log ("The fuck?")); // Error catch for child process
+        }).on("error", (err) => { // Error catch for youtube downloader/file creating write stream
+          Sessions.user(req, req.session).operation = false;
+          reject(new Error(`Conversion failed. ${file.name} was not created/uploaded`));
         }); // Create file at desginated location
 
-	    ytdl(url).pipe(res) // And also send it to user for download
-        .on("finish", () => {
-          Sessions.user(req, req.session).operation = false;
-          return resolve(true);
-        }).on("error", (err) => reject(new Error(`Conversion failed. ${file.name} was not created/uploaded`)));
-	 // ---------------------------------------------------------------------------------
-	  } else { // This could be an image, some audio file, or video from various other sites.
-	    axios.get(url, { responseType: 'stream'}).then( async (result) => {
-	      let stream = result.data;
+// ---------------------------------------------------------------------------------
+  	  } else { // This could be an image, some audio file, or video from various other sites.
+  	    axios.get(url, { responseType: 'stream'}).then( async (result) => {
+  	      let stream = result.data;
 
-	      let approxLength = parseInt(stream.rawHeaders.get('Content-Length', 1)) || stream._readableState.buffer.head.data.length + stream._readableState.buffer.tail.data.length;
-	      let mimetype = stream.rawHeaders.get('Content-Type', 1);
-	      let ext = mime.extension(mimetype);
+  	      let approxLength = parseInt(stream.rawHeaders.get('Content-Length', 1)) || stream._readableState.buffer.head.data.length + stream._readableState.buffer.tail.data.length;
+  	      let mimetype = stream.rawHeaders.get('Content-Type', 1);
+  	      let ext = mime.extension(mimetype);
 
-	      if (!ext || ext.includes('html')) return reject(new Error(`Source URL could not find a valid file. Try linking file address`));
+  	      if (!ext || ext.includes('html')) return reject(new Error(`Source URL could not find a valid file. Try linking file address`));
 
-	      file.name = `${name}.${ext}`;
-	      res.setHeader('content-type', 'application/octet-stream');
-	      res.setHeader('filename', file.name);
-	      res.setHeader('approx-length', parseInt(approxLength));
+  	      file.name = `${name}.${ext}`;
 
-	      stream.pipe(fs.createWriteStream(`${dirpath}/${file.name}`));
-	      stream.pipe(res).on("finish", () => {
-            Sessions.user(req, req.session).operation = false;
-            return resolve(true);
+  	      stream.pipe(fs.createWriteStream(filePath)).on("exit", () => {
+              module.exports.setMetadata(req, res, metadata, filePath).on('exit', function() {
+              res.setHeader('content-type', 'application/octet-stream');
+              res.setHeader('filename', file.name);
+              res.setHeader('approx-length', parseInt(approxLength));
+              fs.createReadStream(filePath).pipe(res).on("error", () => reject(new Error(`Data stream interrupted. File could not be sent for download, refresh directory and locate file to download.`))); // Error catch for file stream to response for download
+              return resolve(true);
+            }).on("error", () => console.log ("The fuck?")); // Error catch for child process
           }).on("error", (err) => reject(new Error(`Conversion failed. ${file.name} was not created/uploaded`)));
-
-	    }).catch( err => {
-	    	Sessions.user(req, req.session).operation = false;
-	    	if (err.response) {
-	          err = err.response;
-	          if (err.statusMessage = 'Bad Request' || err.rawHeaders.get('Content-Type', 1).includes('html'))
-	            return ReportData(req, res, false, {
-	              content: ["Invalid source or false URL. File not found", "Use link from file address/source content for URL"],
-	              type: "error"
-	            });
-	    	}
-	    	return reject(new Error(`Source not valid. File address must be referenced`));
-	    });
-	  } // End Of: If not a YouTube URL
-  	}).then( (result) => {
-  	  Sessions.user(req, req.session).operation = false;
-  	}).catch( (err) => {
-  		console.log('Promise error', err);
-  	    return ReportData(req, res, err);
-	}); // End Of: Main promise error
-  }); // End Of: Reading targeted directory
-  // ---------------------------------------------------------------------------------
+// ---------------------------------------------------------------------------------
+  	    }).catch( err => {
+  	    	Sessions.user(req, req.session).operation = false;
+  	    	if (err.response) {
+  	          err = err.response;
+  	          if (err.statusMessage = 'Bad Request' || err.rawHeaders.get('Content-Type', 1).includes('html'))
+  	            return ReportData(req, res, false, {
+  	              content: ["Invalid source or false URL. File not found", "Use link from file address/source content for URL"],
+  	              type: "error"
+  	            });
+  	    	}
+  	    	return reject(new Error(`Source not valid. File address must be referenced`));
+  	    });
+  	  } // End Of: If not a YouTube URL
+// ---------------------------------------------------------------------------------
+    	}).catch( (err) => {
+    		console.log('Conversion error', err);
+        Sessions.user(req, req.session).operation = false;
+    	  return ReportData(req, res, err);
+  	  }); // End Of: Main promise error
+      Sessions.user(req, req.session).operation = false;
+    }); // End Of: Reading targeted directory
+// ---------------------------------------------------------------------------------
   })),
 
   /* ====================================
@@ -170,7 +188,7 @@ module.exports = {
   try {
 
     let newfolders = req.body.newfolders ? Array.from(req.body.newfolders) : '';
-    //Turns into set, which automatically removes duplicates, but Spreads it, so it converts back into an array
+    // req.body.newfolders is initially a Set (to automatically removes duplicates), convert back to array
     const uploaded = [], failed = [], denied = [];
     let filter = req.files.map( async (file) => {
     	if (file.uploaded) {
@@ -183,11 +201,13 @@ module.exports = {
     	if (file.denied) return denied.push(EntryToHTML(file, 'yellow', '<br>'));
     })
     await Promise.all(filter).then( async () => {
-	    // let uploadedNames = uploaded.length ? uploaded.map( file => file.name) : [];
+
 	    let uploadedNames = Array.from(CapArraySize(uploaded.length ? uploaded.map( file => EntryToHTML(file, 'darkcyan', '<br>')): []));
 	    let result = 'success';
 	    if (failed.length || denied.length) result = uploaded.length ? 'warning' : 'error'
+
 	    let conditionalMessage = `${uploadedNames.length ? 'Uploaded: ' + uploadedNames.join('') + '</span>' : ''} ${newfolders.length ? `along with ${newfolders.length} folders` : ''}<hr> ${denied.length ? denied.join('') + '<br>Was cancelled. Already exist under another user<hr>' : ''} ${failed.length ? failed.join('') + '</span> Failed to upload, may have been altered or do not exist<hr>' : ''}`
+      // Behold. The power of string template literals and the terenary operator.
 	// --------------------------------------------------------------------------
 	    return ReportData (req, res, false, {
 	      content: [conditionalMessage], //This giant mess of a content message is simply for aesthetic purposes and clarity on the client side. Depending on differing results between all file uploads, each outcome displays different message
@@ -208,24 +228,24 @@ module.exports = {
   deleteFiles: app.delete(`/delete/:folder*`, VerifyUser, wrapAsync( async (req, res, next) => {
    try {
 // ------------------------------------------------------------------------------
-    let results = await IterateDelete(req, res, req.body.files);
+    let deleteResults = await IterateDelete(req, res, req.body.files);
     let items = {deleted: [], failed: [], notFound: [], noPermission: []};
     let type = 'success';
 
-    for (let result of results)
+    for (let result of deleteResults)
       items[result.status].push(EntryToHTML(result, 'white', '<br>')); //Each result will have a property that matches one of the four arrays above, so clever little trick. Convert each item to a span element with a representative color.
 
     if (items.failed.length || items.notFound.length || items.noPermission.length)
       type = 'warning' //Determine what theme of message to send
 
 // ------------------------------------------------------------------------------
-    let conditionalMessage = `${items.deleted.length ? "Deleted: " + items.deleted.join('') : ""}${items.noPermission.length ? "<hr>No permission to remove: " + items.noPermission.join('') : ""}${items.notFound.length ? "<hr>" + items.notFound.join('') + "Was either moved, or does not exist" : ""}${items.failed.length ? "<hr>Could not delete: <br>" + items.failed.join('') + "Still in use or being edited" : ""}` // Not necessary for functionality. The above is just to provide user with a better-than-shit explanation of what happened during deletion problems.
+    let conditionalMessage = `${items.deleted.length ? "Deleted: " + items.deleted.join('') : ""}${items.noPermission.length ? "<hr>No permission to remove: " + items.noPermission.join('') : ""}${items.notFound.length ? "<hr>" + items.notFound.join('') + "Was either moved, or does not exist" : ""}${items.failed.length ? "<hr>Could not delete: <br>" + items.failed.join('') + "Still in use or being edited" : ""}` // Not necessary for functionality. The above is just for front-end, to provide user with a shinier-than-shit explanation of any deletion problems that occured.
 
     return ReportData(req, res, false, {
       content: [conditionalMessage],
       type: type,
-      items: results.filter( r => r.status !== 'deleted') || [],
-      deleted: results.filter( r => r.status === 'deleted') || []
+      items: deleteResults.filter( r => r.status !== 'deleted') || [],
+      deleted: deleteResults.filter( r => r.status === 'deleted') || []
     });
    } catch (err) {console.log(err)};
 // ------------------------------------------------------------------------------
